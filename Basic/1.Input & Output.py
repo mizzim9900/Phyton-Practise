db2ba64c9e465bb630a4abae08a2d73b8e1a3568
Sadikul Hasan Zim <77526456+mizzim9900@users.noreply.github.com>
2022-03-26 00:55:01 +0600

    Add files via upload

---
+++ Basic/1.Input & Output.py
@@ -0,0 +1,13 @@
+x=int(input("Take the integer input="))
+z=float(input("Take the float input="))
+y=(input("Take the string or character input="))
+print("X:",x,end='\n')
+print("Z:",z,end='\n')
+print("Y:",y,end='\n')
+print("The output = ",z/x)
+print("The output = %.1f" %(z/x))
+a,b,c="Md","Sadikul","Hasan"
+print(a,b,c,sep="-",end='\n')
+print("Z"+"i"+"m")
+data=[1,2,"w","win",1.3]
+print(data)
